c82f216a36c38dbcde9e9c38fc2f9f19db923832
Jose Celano <josecelano@gmail.com>
2024-05-29 14:02:08 +0100

    fix: run docker demo detached

--- demo/bin/start.sh
+++ demo/bin/start.sh
@@ -4,4 +4,6 @@ USER_ID=${USER_ID:-1000} \
 	TORRUST_INDEX_CONFIG_TOML=$(cat config-index.local.toml) \
 	TORRUST_TRACKER_CONFIG_TOML=$(cat config-tracker.local.toml) \
 	TORRUST_TRACKER_CONFIG_OVERRIDE_HTTP_API__ACCESS_TOKENS__ADMIN=${TORRUST_TRACKER_CONFIG_OVERRIDE_HTTP_API__ACCESS_TOKENS__ADMIN:-MyAccessToken} \
-	docker compose up --build
+	docker compose up --build -d
+
+
